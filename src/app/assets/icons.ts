1678ee645e4a30b0870522eed2ec115fa1098860
notS0LD13R <joelsebin@gmail.com>
2023-09-17 23:15:09 +0530

    chore(assets) : added resources

---
+++ src/app/assets/icons.ts
@@ -0,0 +1,41 @@
+import { AiTwotoneShop as Shop } from "react-icons/ai";
+import {
+    BiSolidTachometer as Meter,
+    BiSolidDownArrow as Dropdown,
+} from "react-icons/bi";
+import { HiMail as Mail, HiMenu as Menu } from "react-icons/hi";
+import { IoIosChatbubbles as Chat } from "react-icons/io";
+import { HiMiniSquares2X2 as Grid } from "react-icons/hi2";
+import {
+    RiContactsBook2Fill as Contact,
+    RiSettings4Fill as Settings,
+    RiCheckboxMultipleFill as Checkbox,
+} from "react-icons/ri";
+import {
+    BsCalendarFill as Calendar,
+    BsPersonVideo3 as Course,
+} from "react-icons/bs";
+import {
+    LiaFileInvoiceSolid as Invoice,
+    LiaSearchSolid as Search,
+} from "react-icons/lia";
+import { FaBell as Bell, FaFolder as Folder } from "react-icons/fa";
+const icons = {
+    Menu,
+    Meter,
+    Mail,
+    Chat,
+    Grid,
+    Contact,
+    Calendar,
+    Course,
+    Shop,
+    Invoice,
+    Settings,
+    Search,
+    Bell,
+    Checkbox,
+    Folder,
+    Dropdown,
+};
+export default icons;
---
+++ src/app/assets/index.ts
@@ -0,0 +1,11 @@
+import India from "./Flags/Flag-India.webp";
+import USA from "./Flags/download.png";
+
+export const flags = {
+    India,
+    USA,
+};
+
+import profileDP from "./dp.jpg";
+
+export const profile = { profileDP };
